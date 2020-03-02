e43bc9e3cf2e0f23a06745e927c8b60d59a54fa2
BRUNO SOUZA <BA7308@sescbahia.com.br>
2020-03-02 17:24:45 -0300

    Do some refactorings

--- GapiCodegen/Generatables/StructBase.cs
+++ GapiCodegen/Generatables/StructBase.cs
@@ -21,293 +21,301 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
-
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
 using GapiCodegen.Interfaces;
 using GapiCodegen.Utils;
 
-namespace GapiCodegen.Generatables {
-	public abstract class StructBase : ClassBase, IManualMarshaler {
-	
-		IList<StructField> fields = new List<StructField> ();
-		bool need_read_native = false;
+namespace GapiCodegen.Generatables
+{
+    /// <summary>
+    /// Abstract base class for types that will be translated to C# structs.
+    /// </summary>
+    public abstract class StructBase : ClassBase, IManualMarshaler
+    {
+        private readonly IList<StructField> _fields = new List<StructField>();
+        private bool _needReadNative;
 
-		protected StructBase (XmlElement namespaceElement, XmlElement element) : base (namespaceElement, element)
-		{
-			foreach (XmlNode node in element.ChildNodes) {
+        protected StructBase(XmlElement namespaceElement, XmlElement element) : base(namespaceElement, element)
+        {
+            foreach (XmlNode node in element.ChildNodes)
+            {
+                if (!(node is XmlElement)) continue;
+                var member = (XmlElement)node;
 
-				if (!(node is XmlElement)) continue;
-				XmlElement member = (XmlElement) node;
+                switch (node.Name)
+                {
+                    case Constants.Field:
+                        _fields.Add(new StructField(member, this));
+                        break;
 
-				switch (node.Name) {
-				case "field":
-					fields.Add (new StructField (member, this));
-					break;
+                    case Constants.Callback:
+                        Statistics.IgnoreCount++;
+                        break;
 
-				case "callback":
-					Statistics.IgnoreCount++;
-					break;
+                    default:
+                        if (!IsNodeNameHandled(node.Name)){
+                            Console.WriteLine($"Unexpected node {node.Name} in {CName}");}
+                        break;
+                }
+            }
+        }
 
-				default:
-					if (!IsNodeNameHandled (node.Name))
-						Console.WriteLine ("Unexpected node " + node.Name + " in " + CName);
-					break;
-				}
-			}
-		}
+        private bool DisableNew => Element.GetAttributeAsBoolean(Constants.DisableNew);
 
-		public override string DefaultValue {
-			get {
-				return QualifiedName + ".Zero";
-			}
-		}
+        public override string DefaultValue => $"{QualifiedName}.Zero";
 
-		public override string MarshalType {
-			get {
-				return "IntPtr";
-			}
-		}
+        public override string MarshalType => "IntPtr";
 
-		public override string AssignToName {
-			get { throw new NotImplementedException (); }
-		}
+        public override string AssignToName => throw new NotImplementedException();
 
-		public override string CallByName ()
-		{
-			return "this_as_native";
-		}
+        public override string CallByName()
+        {
+            return "this_as_native";
+        }
 
-		public override string CallByName (string var)
-		{
-			return var + "_as_native";
-		}
+        public override string CallByName(string varName)
+        {
+            return $"{varName}_as_native";
+        }
 
-		public override string FromNative (string varName)
-		{
-			if (DisableNew)
-				return varName + " == IntPtr.Zero ? " + QualifiedName + ".Zero : (" + QualifiedName + ") System.Runtime.InteropServices.Marshal.PtrToStructure (" + varName + ", typeof (" + QualifiedName + "))";
-			else
-				return QualifiedName + ".New (" + varName + ")";
-		}
-		
-		public string AllocNative (string var)
-		{
-			return "GLib.Marshaller.StructureToPtrAlloc (" + var + ")";
-		}
+        public override string FromNative(string varName)
+        {
+            if (DisableNew)
+                return string.Format(
+                    "{0} == IntPtr.Zero ? {1}.Zero : ({1}) System.Runtime.InteropServices.Marshal.PtrToStructure ({0}, typeof ({1}))",
+                    varName, QualifiedName);
+            
+            return $"{QualifiedName}.New ({varName})";
+        }
 
-		public string ReleaseNative (string var)
-		{
-			return "Marshal.FreeHGlobal (" +var + ")";
-		}
+        public string AllocNative(string var)
+        {
+            return $"GLib.Marshaller.StructureToPtrAlloc ({var})";
+        }
 
-		private bool DisableNew {
-			get {
-				return Element.GetAttributeAsBoolean ("disable_new");
-			}
-		}
+        public string ReleaseNative(string var)
+        {
+            return $"Marshal.FreeHGlobal ({var})";
+        }
 
-		public virtual bool Union {
-			get {
-				return false;
-			}
-		}
+        public virtual bool Union => false;
 
-		protected void GenEqualsAndHash (StreamWriter sw)
-		{
-			int bitfields = 0;
-			bool need_field = true;
-			StringBuilder hashcode = new StringBuilder ();
-			StringBuilder equals = new StringBuilder ();
+        public override bool CanGenerateAbiStruct(LogWriter logWriter)
+        {
+            logWriter.Info("Not generating any ABI structs for managed structures");
 
-			hashcode.Append ("this.GetType ().FullName.GetHashCode ()");
-			equals.Append ("true");
+            return false;
+        }
 
-			foreach (StructField field in fields) {
-				if (field.IsPadding || field.Hidden)
-					continue;
-				if (field.IsBitfield) {
-					if (need_field) {
-						equals.Append (" && _bitfield");
-						equals.Append (bitfields);
-						equals.Append (".Equals (other._bitfield");
-						equals.Append (bitfields);
-						equals.Append (")");
-						hashcode.Append (" ^ ");
-						hashcode.Append ("_bitfield");
-						hashcode.Append (bitfields++);
-						hashcode.Append (".GetHashCode ()");
-						need_field = false;
-					}
-				} else {
-					need_field = true;
-					equals.Append (" && ");
-					equals.Append (field.EqualityName);
-					equals.Append (".Equals (other.");
-					equals.Append (field.EqualityName);
-					equals.Append (")");
-					hashcode.Append (" ^ ");
-					hashcode.Append (field.EqualityName);
-					hashcode.Append (".GetHashCode ()");
-				}
-			}
+        public override void Generate(GenerationInfo generationInfo)
+        {
+            var needClose = false;
 
-			if (!Element.GetAttributeAsBoolean ("noequals")) {
-				sw.WriteLine ("\t\tpublic bool Equals ({0} other)", Name);
-				sw.WriteLine ("\t\t{");
-				sw.WriteLine ("\t\t\treturn {0};", equals.ToString ());
-				sw.WriteLine ("\t\t}");
-				sw.WriteLine ();
-			}
-			sw.WriteLine ("\t\tpublic override bool Equals (object other)");
-			sw.WriteLine ("\t\t{");
-			sw.WriteLine ("\t\t\treturn other is {0} && Equals (({0}) other);", Name);
-			sw.WriteLine ("\t\t}");
-			sw.WriteLine ();
-			if (Element.GetAttributeAsBoolean ("nohash"))
-				return;
-			sw.WriteLine ("\t\tpublic override int GetHashCode ()");
-			sw.WriteLine ("\t\t{");
-			sw.WriteLine ("\t\t\treturn {0};", hashcode.ToString ());
-			sw.WriteLine ("\t\t}");
-			sw.WriteLine ();
+            if (generationInfo.Writer == null)
+            {
+                generationInfo.Writer = generationInfo.OpenStream(Name, Namespace);
+                needClose = true;
+            }
 
-		}
+            var sw = generationInfo.Writer;
 
-		protected new void GenFields (GenerationInfo gen_info)
-		{
-			int bitfields = 0;
-			bool need_field = true;
-			StreamWriter sw = gen_info.Writer;
+            sw.WriteLine($"namespace {Namespace} {{");
+            sw.WriteLine();
+            sw.WriteLine("\tusing System;");
+            sw.WriteLine("\tusing System.Collections;");
+            sw.WriteLine("\tusing System.Collections.Generic;");
+            sw.WriteLine("\tusing System.Runtime.InteropServices;");
+            sw.WriteLine();
 
-			foreach (StructField field in fields) {
-				if (Union)
-					sw.WriteLine ("\t\t[FieldOffset(0)]");
-				if (field.IsBitfield) {
-					if (need_field) {
-						sw.WriteLine ("\t\tprivate uint _bitfield{0};\n", bitfields++);
-						need_field = false;
-					}
-				} else
-					need_field = true;
-				field.Generate (gen_info, "\t\t");
-			}
-		}
+            sw.WriteLine("#region Autogenerated code");
 
-		public override bool Validate ()
-		{
-			LogWriter log = new LogWriter (QualifiedName);
-			foreach (StructField field in fields) {
-				if (!field.Validate (log)) {
-					if (!field.IsPointer)
-						return false;
-				}
-			}
+            if (IsDeprecated)
+                sw.WriteLine("\t[Obsolete]");
 
-			return base.Validate ();
-		}
+            sw.WriteLine(Union ? "\t[StructLayout(LayoutKind.Explicit)]" : "\t[StructLayout(LayoutKind.Sequential)]");
 
-		public override bool CanGenerateAbiStruct(LogWriter logWriter) {
-			logWriter.Info("Not generating any ABI structs for managed structures");
+            var access = IsInternal ? "internal" : "public";
+            sw.WriteLine("\t{1} partial struct {0} : IEquatable<{0}> {{", Name, access);
+            sw.WriteLine();
 
-			return false;
-		}
+            _needReadNative = false;
 
-		public override void Generate (GenerationInfo generationInfo)
-		{
-			bool need_close = false;
+            GenerateFields(generationInfo);
+            sw.WriteLine();
 
-			if (generationInfo.Writer == null) {
-				generationInfo.Writer = generationInfo.OpenStream (Name, Namespace);
-				need_close = true;
-			}
+            GenerateConstructors(generationInfo);
+            GenerateMethods(generationInfo, null, this);
 
-			StreamWriter sw = generationInfo.Writer;
-			
-			sw.WriteLine ("namespace " + Namespace + " {");
-			sw.WriteLine ();
-			sw.WriteLine ("\tusing System;");
-			sw.WriteLine ("\tusing System.Collections;");
-			sw.WriteLine ("\tusing System.Collections.Generic;");
-			sw.WriteLine ("\tusing System.Runtime.InteropServices;");
-			sw.WriteLine ();
-			
-			sw.WriteLine ("#region Autogenerated code");
-			if (IsDeprecated)
-				sw.WriteLine ("\t[Obsolete]");
-			if (Union)
-				sw.WriteLine ("\t[StructLayout(LayoutKind.Explicit)]");
-			else
-				sw.WriteLine ("\t[StructLayout(LayoutKind.Sequential)]");
-			string access = IsInternal ? "internal" : "public";
-			sw.WriteLine ("\t" + access + " partial struct {0} : IEquatable<{0}> {{", Name);
-			sw.WriteLine ();
+            if (_needReadNative)
+                GenerateReadNative(sw);
+            
+            GenerateEqualsAndHash(sw);
 
-			need_read_native = false;
-			GenFields (generationInfo);
-			sw.WriteLine ();
-			GenerateConstructors (generationInfo);
-			GenerateMethods (generationInfo, null, this);
-			if (need_read_native)
-				GenReadNative (sw);
-			GenEqualsAndHash (sw);
+            if (!needClose)
+                return;
 
-			if (!need_close)
-				return;
+            sw.WriteLine("#endregion");
 
-			sw.WriteLine ("#endregion");
+            sw.WriteLine("\t}");
+            sw.WriteLine("}");
+            sw.Close();
+            generationInfo.Writer = null;
+        }
 
-			sw.WriteLine ("\t}");
-			sw.WriteLine ("}");
-			sw.Close ();
-			generationInfo.Writer = null;
-		}
-		
-		protected override void GenerateConstructors (GenerationInfo generationInfo)
-		{
-			StreamWriter sw = generationInfo.Writer;
+        private new void GenerateFields(GenerationInfo generationInfo)
+        {
+            var sw = generationInfo.Writer;
+            var bitfields = 0;
+            var needField = true;
 
-			sw.WriteLine ("\t\tpublic static {0} Zero = new {0} ();", QualifiedName);
-			sw.WriteLine();
-			if (!DisableNew) {
-				sw.WriteLine ("\t\tpublic static " + QualifiedName + " New(IntPtr raw) {");
-				sw.WriteLine ("\t\t\tif (raw == IntPtr.Zero)");
-				sw.WriteLine ("\t\t\t\treturn {0}.Zero;", QualifiedName);
-				sw.WriteLine ("\t\t\treturn ({0}) Marshal.PtrToStructure (raw, typeof ({0}));", QualifiedName);
-				sw.WriteLine ("\t\t}");
-				sw.WriteLine ();
-			}
+            foreach (var field in _fields)
+            {
+                if (Union)
+                    sw.WriteLine("\t\t[FieldOffset(0)]");
 
-			foreach (Ctor ctor in Constructors)
-				ctor.IsStatic = true;
+                if (field.IsBitfield)
+                {
+                    if (needField)
+                    {
+                        sw.WriteLine("\t\tprivate uint _bitfield{0};\n", bitfields++);
+                        needField = false;
+                    }
+                }
+                else
+                    needField = true;
 
-			base.GenerateConstructors (generationInfo);
-		}
+                field.Generate(generationInfo, "\t\t");
+            }
+        }
 
-		void GenReadNative (StreamWriter sw)
-		{
-			sw.WriteLine ("\t\tstatic void ReadNative (IntPtr native, ref {0} target)", QualifiedName);
-			sw.WriteLine ("\t\t{");
-			sw.WriteLine ("\t\t\ttarget = New (native);");
-			sw.WriteLine ("\t\t}");
-			sw.WriteLine ();
-		}
+        protected override void GenerateConstructors(GenerationInfo generationInfo)
+        {
+            var sw = generationInfo.Writer;
 
-		public override void Prepare (StreamWriter sw, string indent)
-		{
-			sw.WriteLine (indent + "IntPtr this_as_native = System.Runtime.InteropServices.Marshal.AllocHGlobal (System.Runtime.InteropServices.Marshal.SizeOf (this));");
-			sw.WriteLine (indent + "System.Runtime.InteropServices.Marshal.StructureToPtr (this, this_as_native, false);");
-		}
+            sw.WriteLine("\t\tpublic static {0} Zero = new {0} ();", QualifiedName);
+            sw.WriteLine();
 
-		public override void Finish (StreamWriter sw, string indent)
-		{
-			need_read_native = true;
-			sw.WriteLine (indent + "ReadNative (this_as_native, ref this);");
-			sw.WriteLine (indent + "System.Runtime.InteropServices.Marshal.FreeHGlobal (this_as_native);");
-		}
-	}
+            if (!DisableNew)
+            {
+                sw.WriteLine($"\t\tpublic static {QualifiedName} New(IntPtr raw) {{");
+                sw.WriteLine("\t\t\tif (raw == IntPtr.Zero)");
+                sw.WriteLine("\t\t\t\treturn {0}.Zero;", QualifiedName);
+                sw.WriteLine("\t\t\treturn ({0}) Marshal.PtrToStructure (raw, typeof ({0}));", QualifiedName);
+                sw.WriteLine("\t\t}");
+                sw.WriteLine();
+            }
+
+            foreach (var ctor in Constructors)
+                ctor.IsStatic = true;
+
+            base.GenerateConstructors(generationInfo);
+        }
+
+        private void GenerateReadNative(TextWriter sw)
+        {
+            sw.WriteLine("\t\tstatic void ReadNative (IntPtr native, ref {0} target)", QualifiedName);
+            sw.WriteLine("\t\t{");
+            sw.WriteLine("\t\t\ttarget = New (native);");
+            sw.WriteLine("\t\t}");
+            sw.WriteLine();
+        }
+
+        protected void GenerateEqualsAndHash(StreamWriter sw)
+        {
+            var equals = new StringBuilder("true");
+            var hashCode = new StringBuilder("this.GetType ().FullName.GetHashCode ()");
+
+            var bitfields = 0;
+            var needField = true;
+
+            foreach (var field in _fields)
+            {
+                if (field.IsPadding || field.Hidden)
+                    continue;
+
+                if (field.IsBitfield)
+                {
+                    if (!needField) continue;
+
+                    equals.Append(" && _bitfield");
+                    equals.Append(bitfields);
+                    equals.Append(".Equals (other._bitfield");
+                    equals.Append(bitfields);
+                    equals.Append(")");
+
+                    hashCode.Append(" ^ ");
+                    hashCode.Append("_bitfield");
+                    hashCode.Append(bitfields++);
+                    hashCode.Append(".GetHashCode ()");
+
+                    needField = false;
+                }
+                else
+                {
+                    equals.Append(" && ");
+                    equals.Append(field.EqualityName);
+                    equals.Append(".Equals (other.");
+                    equals.Append(field.EqualityName);
+                    equals.Append(")");
+
+                    hashCode.Append(" ^ ");
+                    hashCode.Append(field.EqualityName);
+                    hashCode.Append(".GetHashCode ()");
+
+                    needField = true;
+                }
+            }
+
+            if (!Element.GetAttributeAsBoolean("noequals"))
+            {
+                sw.WriteLine("\t\tpublic bool Equals ({0} other)", Name);
+                sw.WriteLine("\t\t{");
+                sw.WriteLine("\t\t\treturn {0};", equals);
+                sw.WriteLine("\t\t}");
+                sw.WriteLine();
+            }
+
+            sw.WriteLine("\t\tpublic override bool Equals (object other)");
+            sw.WriteLine("\t\t{");
+            sw.WriteLine("\t\t\treturn other is {0} && Equals (({0}) other);", Name);
+            sw.WriteLine("\t\t}");
+            sw.WriteLine();
+
+            if (Element.GetAttributeAsBoolean("nohash"))
+                return;
+
+            sw.WriteLine("\t\tpublic override int GetHashCode ()");
+            sw.WriteLine("\t\t{");
+            sw.WriteLine("\t\t\treturn {0};", hashCode);
+            sw.WriteLine("\t\t}");
+            sw.WriteLine();
+        }
+
+        public override void Prepare(StreamWriter sw, string indent)
+        {
+            sw.WriteLine(
+                $"{indent}IntPtr this_as_native = System.Runtime.InteropServices.Marshal.AllocHGlobal (System.Runtime.InteropServices.Marshal.SizeOf (this));");
+            sw.WriteLine($"{indent}System.Runtime.InteropServices.Marshal.StructureToPtr (this, this_as_native, false);");
+        }
+
+        public override void Finish(StreamWriter sw, string indent)
+        {
+            _needReadNative = true;
+            
+            sw.WriteLine($"{indent}ReadNative (this_as_native, ref this);");
+            sw.WriteLine($"{indent}System.Runtime.InteropServices.Marshal.FreeHGlobal (this_as_native);");
+        }
+
+        public override bool Validate()
+        {
+            var log = new LogWriter(QualifiedName);
+
+            return _fields.Where(field => !field.Validate(log)).All(field => field.IsPointer) && base.Validate();
+        }
+    }
 }
-
